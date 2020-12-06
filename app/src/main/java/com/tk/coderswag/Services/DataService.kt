0b28b87b54fc7e8be5aadafd946ab4b0abcf6176
Tufan Kesen <tufankesen@gmail.com>
2020-12-06 16:46:58 +0300

    lesson 04

--- app/src/main/java/com/tk/coderswag/Services/DataService.kt
+++ app/src/main/java/com/tk/coderswag/Services/DataService.kt
@@ -8,7 +8,19 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
-            Category("DIGITAL", "digitalgoodsimage")
+            Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf(
